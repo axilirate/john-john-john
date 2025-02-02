fa7fbeb4cad46762dd294754028b7fcd3a992289
axilirate <56825186+axilirate@users.noreply.github.com>
2025-02-02 11:20:34 +0200

    somewhat working worm

--- godot_project/scenes/entities/worm/worm.gd
+++ godot_project/scenes/entities/worm/worm.gd
@@ -1,26 +1,98 @@
 class_name Worm extends CharacterBody2D
 
+@export var ground_detection_area: Area2D
+@export var body_line: Line2D
+
+@export var segment_length: int = 3
+@export var points: int = 10
+
+
+@onready var in_ground: bool = is_in_ground()
 
 
 var acceleration: float = 0.01
-var speed: float = 175.0
+var gravity: float = 4.5
+var speed: float = 125.0
 
 var target_dir: Vector2
 
 
-#TODO: give it 2 states, over ground and under ground, while over ground, 
-# make it be effected by gravity, while in the ground, make it turn towards the player.
+
+func _ready() -> void:
+	body_line.points.clear()
+	for point in points:
+		body_line.add_point(Vector2.ZERO, point)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	in_ground = true
+
+func _on_ground_detection_area_body_exited(body: Node2D) -> void:
+	in_ground = false
+
+
+
+func _process(delta: float) -> void:
+	_process_body_line()
+
 
 
 func _physics_process(delta: float) -> void:
 	if not is_instance_valid(World.player):
 		return
 	
-	
-	var new_target_dir: Vector2 = global_position.direction_to(World.player.global_position)
-	target_dir = target_dir.lerp(new_target_dir, delta * 3.0).normalized()
-	
-	velocity = velocity.lerp(target_dir * speed, acceleration)
-	
+	_process_velocity(delta)
 	
 	move_and_slide()
+
+
+
+
+func _process_body_line() -> void:
+	body_line.set_point_position(0, global_position)
+	
+	for idx in body_line.get_point_count():
+		if idx == 0:
+			continue
+		
+		var last_point_pos: Vector2 = body_line.get_point_position(idx - 1)
+		var curr_point_pos: Vector2 = body_line.get_point_position(idx)
+		var new_point_pos: Vector2 = constrain_distance(curr_point_pos, last_point_pos, segment_length)
+		
+		body_line.set_point_position(idx, new_point_pos)
+		
+		#sprite.look_at(sprite.global_position + target_dir)
+
+
+
+func _process_velocity(delta: float) -> void:
+	if not in_ground:
+		velocity.y += gravity
+		velocity.y = min(125, velocity.y)
+		target_dir = velocity.normalized()
+		return
+	
+	
+	var new_target_dir: Vector2 = global_position.direction_to(World.player.global_position)
+	target_dir = target_dir.lerp(new_target_dir, delta * 10).normalized()
+	
+	velocity = target_dir * speed
+
+
+
+
+
+
+func is_in_ground() -> bool:
+	if ground_detection_area.get_overlapping_bodies().size():
+		return true
+	return false
+
+
+
+func constrain_distance(current: Vector2, target: Vector2, max_dist: float) -> Vector2:
+	var direction = current.direction_to(target)
+	var distance = current.distance_to(target)
+	if distance > max_dist:
+		return target - direction * max_dist
+	return current
--- godot_project/scenes/environment/world/world.gd
+++ godot_project/scenes/environment/world/world.gd
@@ -1,5 +1,8 @@
 class_name World extends Node2D
 
+@export var pixel_viewport: SubViewport
+@export var pixel_camera: Camera2D
+
 static var player: Player = null
 
 
@@ -7,4 +10,12 @@ static var player: Player = null
 
 
 func _ready() -> void:
+	get_viewport().set_canvas_cull_mask_bit(1, false)
+	pixel_viewport.world_2d = get_world_2d()
 	player = $Player
+
+
+
+func _process(delta: float) -> void:
+	pixel_camera.global_position = player.camera.global_position
+	pixel_viewport.size_2d_override = get_viewport().size / 4
