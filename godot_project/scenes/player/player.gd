30453b5a86b09b1d6dcf7234061be719efa1052e
axilirate <56825186+axilirate@users.noreply.github.com>
2025-02-19 12:19:30 +0200

    movement tech

--- godot_project/scenes/player/player.gd
+++ godot_project/scenes/player/player.gd
@@ -1,7 +1,7 @@
 class_name Player extends CharacterController
 
 
-@onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var animation_tree: AnimationTree = %AnimationTree
 
 @export_category("Nodes")
 @export var sprite: Sprite2D
@@ -15,8 +15,8 @@ var flipped: bool = false
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
+	_process_animation_blends()
 	_process_look_dir()
-	
 	_process_sprite()
 
 
@@ -28,36 +28,40 @@ func _process_look_dir() -> void:
 	
 	if velocity.x < 0.0:
 		flipped = true
+	
+	
+	if is_on_wall() and air_time[0] > 0.1:
+		var normal: Vector2 = get_wall_normal()
+		flipped = normal.x == -1
+
 
 
 
 func _process_sprite() -> void:
-	animation_player.play(get_target_animation())
-	
-	sprite.global_position = global_position + Vector2(-5, -9)
+	sprite.global_position = global_position + Vector2(0.0, -3.5)
 	
 	if is_on_floor():
-		sprite.global_position.y = round(sprite.global_position.y)
+		sprite.global_position.y = round(sprite.global_position.y) + 0.5
 	
 	if is_equal_approx(velocity.x, 0.0):
 		sprite.global_position.x = round(sprite.global_position.x)
 	
 	sprite.flip_h = flipped
 	sprite.offset.x = 0
+
+
+
+
+func _process_animation_blends() -> void:
+	var is_running: bool = not is_equal_approx(velocity.x, 0.0) and not get_horizontal_input() == 0
 	
-	if flipped:
-		sprite.offset.x = 1
-
-
-
-
-
-
-func get_target_animation() -> String:
-	if not is_on_floor():
-		return "air"
+	if not animation_tree["parameters/air_jump_one_shot/active"]:
+		animation_tree["parameters/air_jump_blend/blend_amount"] = int(flipped)
 	
-	if not is_equal_approx(velocity.x, 0.0) and not get_horizontal_input() == 0:
-		return "run"
-	
-	return "idle"
+	animation_tree["parameters/air_blend/blend_amount"] = int(not is_on_floor())
+	animation_tree["parameters/run_blend/blend_amount"] = int(is_running)
+
+
+func _on_jumped(jump_type: CharacterController.JumpType) -> void:
+	match jump_type:
+		CharacterController.JumpType.AIR: animation_tree["parameters/air_jump_one_shot/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
--- godot_project/scripts/nodes/character_controller.gd
+++ godot_project/scripts/nodes/character_controller.gd
@@ -1,12 +1,23 @@
 class_name CharacterController extends CharacterBody2D
 
+enum JumpType {GROUND, AIR, WALL}
+
+signal jumped(jump_type: JumpType)
+signal landed
 
 
+const CONTROL_RECHARGE_SPEED: float = 3
+
+const TIME_TO_MAX_SPEED: float = 0.1
+const TIME_TO_STOP: float = 0.1
 
 const TIME_TO_MAX_HEIGHT: float = 0.3
-const DEFAULT_SPEED: float = 45
+const DEFAULT_SPEED: float = 57
 const MAX_HEIGHT: float = 21
 const MIN_HEIGHT: float = 7
+
+const WALL_JUMP_FORCE: float = 75
+
 
 
 var jump_force: float = calculate_jump_force()
@@ -14,54 +25,149 @@ var gravity: float = calculate_gravity()
 var time_to_min_height: float = calculate_time_to_min_height()
 
 
-
 var speed: float = DEFAULT_SPEED
-var air_time: float = 0.0
+var acceleration: float = 0.0
+var friction: float = 0.0
+var max_air_jumps: int = 1
 
 
+var wall_time: PackedFloat32Array = [0.0, 0.0]
+var air_time: PackedFloat32Array = [0.0, 0.0]
+var air_jumps_left: float = max_air_jumps
+var control: Vector2 = Vector2.ONE
+
+
+
+func _set_speed(new_speed: float) -> void:
+	acceleration = speed / TIME_TO_MAX_SPEED
+	friction = speed / TIME_TO_STOP
+	speed = new_speed
+
+
+
+func _ready() -> void:
+	landed.connect(_on_landed)
+	_set_speed(DEFAULT_SPEED)
+
+
+func _on_landed() -> void:
+	air_jumps_left = max_air_jumps
 
 
 
 func _physics_process(delta: float) -> void:
+	_process_wall_time(delta)
 	_process_air_time(delta)
 	_process_velocity(delta)
-
-
-
-
-
+	_process_control(delta)
 
 
 func _process_velocity(delta: float) -> void:
 	_process_gravity(delta)
-	_process_movement()
+	_process_movement(delta)
 	_process_jump()
+	_process_land()
 	
 	move_and_slide()
 
 
 
-func _process_air_time(delta: float) -> void:
-	if not is_on_floor():
-		air_time += delta
+func _process_control(delta: float) -> void:
+	control = control.move_toward(Vector2.ONE, CONTROL_RECHARGE_SPEED * delta)
+
+
+
+func _process_wall_time(delta: float) -> void:
+	wall_time[1] = wall_time[0]
+	
+	if is_on_wall():
+		wall_time[0] += delta
 		return
-	air_time = 0.0
+	
+	wall_time[0] = 0.0
+
+
+func _process_air_time(delta: float) -> void:
+	air_time[1] = air_time[0]
+	
+	if not is_on_floor():
+		air_time[0] += delta
+		return
+	
+	air_time[0] = 0.0
 
 
 
-func _process_movement() -> void:
-	velocity.x = get_horizontal_input() * speed
+
+
+func _process_movement(delta: float) -> void:
+	var horizontal_input: int = get_horizontal_input()
+	
+	if not horizontal_input == 0:
+		var target_speed: float = horizontal_input * acceleration * maxf(control.x, 0.0) * delta
+		match horizontal_input:
+			1: 
+				var remainder: float = speed - velocity.x
+				target_speed = minf(target_speed, remainder)
+			
+			-1: 
+				var remainder: float = -velocity.x - speed
+				target_speed = maxf(target_speed, remainder)
+		
+		
+		velocity.x += target_speed
+		return
+	
+	
+	if air_time[0] == 0.0:
+		velocity.x = move_toward(velocity.x, 0.0, friction * delta)
+
+
 
 
 
 func _process_jump() -> void:
-	if not is_on_floor():
-		return
+	var in_air: bool = air_time[0] > 0.1
 	
 	if not Input.is_action_just_pressed("jump"):
 		return
 	
+	if in_air:
+		if wall_time[0] > 0.1:
+			_jump(JumpType.WALL)
+			return
+		
+		if air_jumps_left > 0:
+			_jump(JumpType.AIR)
+			return
+		return
+	
+	_jump(JumpType.GROUND)
+
+
+
+
+func _jump(jump_type: JumpType) -> void:
 	velocity.y = -jump_force
+	match jump_type:
+		JumpType.AIR: air_jumps_left -= 1
+		
+		JumpType.WALL:
+			var wall_dir: int = get_wall_normal().x
+			velocity.x += WALL_JUMP_FORCE * wall_dir
+			control.x -= 0.5
+	
+	jumped.emit(jump_type)
+
+
+
+
+
+
+
+func _process_land() -> void:
+	if air_time[0] == 0.0 and air_time[1] > 0.0:
+		landed.emit()
 
 
 
@@ -71,15 +177,21 @@ func _process_gravity(delta: float) -> void:
 		velocity.y = 0.0
 		return 
 	
+	var limit: float = jump_force * 1.45
 	var new_gravity: float = gravity
 	
-	if air_time > time_to_min_height:
+	if air_time[0] > time_to_min_height:
 		if not Input.is_action_pressed("jump"):
-			new_gravity *= 2
+			new_gravity = gravity * 2
+	
+	if is_on_wall() and velocity.y > 0:
+		new_gravity = gravity * 0.5
+		limit = gravity * 0.1
 	
 	velocity.y += new_gravity * delta
-	
-	velocity.y = clampf(velocity.y, -jump_force * 2, jump_force * 2)
+	velocity.y = clampf(velocity.y, -limit, limit)
+
+
 
 
 
@@ -98,20 +210,14 @@ func get_horizontal_input() -> int:
 
 
 
+
 func calculate_gravity():
 	return (2 * MAX_HEIGHT) / pow(TIME_TO_MAX_HEIGHT, 2)
 
 func calculate_jump_force():
 	return (2 * MAX_HEIGHT) / (TIME_TO_MAX_HEIGHT)
 
-
 func calculate_time_to_min_height() -> float:
 	var discriminant = jump_force * jump_force - 2.0 * gravity * MIN_HEIGHT
-	
-	if discriminant < 0:
-		return -1.0  # Cannot reach this height
-	
 	var sqrt_disc = sqrt(discriminant)
-	var t = (jump_force - sqrt_disc) / gravity
-	
-	return t if t >= 0 else -1.0  # Only return positive time
+	return (jump_force - sqrt_disc) / gravity
